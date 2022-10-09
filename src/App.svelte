79f219982b5557d1fe7d51755a911332307f365a
Filipe Moreira Coelho <60034638+Filipe-m@users.noreply.github.com>
2022-10-08 22:36:17 -0300

    2.0 final

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,20 @@
   import Clock from './lib/Clock.svelte';
   import Temperature from './lib/Temperature.svelte';
   import Searchbar from './lib/Searchbar.svelte';
+  import { fade } from 'svelte/transition';
+	import { slide } from 'svelte/transition';
+	import { sineInOut } from 'svelte/easing';
+  import { quintOut } from 'svelte/easing';
+
+  let openSide = false
+
+  let sites = [
+    {name : 'Youtube', img:'www.youtube.com', link: 'https://www.youtube.com/'},
+    {name : 'Whatsapp', img: 'web.whatsapp.com', link: 'https://web.whatsapp.com/'},
+    {name: 'Github', img:'github.com/Filipe-m?tab=repositories', link: 'https://github.com/Filipe-m?tab=repositories'},
+    {name: 'Twitter', img: 'twitter.com', link: 'https://twitter.com/home'},
+    {name: 'Instagram' , img: 'instagram.com', link: 'https://www.instagram.com/'}
+  ]
 
   let internet = false
   const check = () => {
@@ -24,9 +38,54 @@
   <div class="search">
     <Searchbar/>
   </div>
+  {#if openSide}
+  <div class="sidebar" transition:slide="{{delay: 50, duration: 500, easing: quintOut }}">
+    <div class="bar">
+      <div class="close-btn">
+        <button class="btn" on:click={()=> openSide = false}>✕</button>
+      </div>
+      <div class="links">
+        <div class="apps">
+          {#each sites as site }
+          <a class="app" href={site.link}><img class="img" src='https://icon.horse/icon/{site.img}' alt={site.name}></a>
+          {/each}
+        </div>
+      </div>
+    </div>
+  </div>
+  {/if}
+  {#if openSide == false }
+  <button transition:fade="{{delay: 0, duration: 200}}" class="sidebar-btn" on:click={() => openSide = true}>☰</button>
+  {/if}
 </main>
 
 <style>
+  .sidebar-btn{
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: transparent;
+    border-style: none;
+    color: white;
+    font-size: 1.5rem;
+    width: 3rem;
+    height: 3rem;
+    border-radius: 10px;
+  }
+  .sidebar-btn:hover{
+    cursor: pointer;
+    background-color: rgba(0, 0, 0, 0.473);
+  }
+  
+  .sidebar{
+    position: fixed;
+    left: 0.3rem;
+    display: flex;
+    align-items: center;
+    height: 100%;
+    z-index: 1;
+  }
+
   .temp{
     position: absolute;
     padding: 0rem;
---
+++ src/lib/SideBar.svelte
@@ -0,0 +1,68 @@
+<script>
+
+</script>
+
+<div class="bar">
+  <div class="close-btn">
+    <button class="btn">✕</button>
+  </div>
+  <div class="links">
+    <div class="apps">
+      <a class="app" href="youtube.com"><img class="img" src="https://rotony.com.br/wp-content/uploads/2021/09/free-youtube-logo-icon-2431-thumb.png" alt="YT"></a>
+    </div>
+    <div class="apps">
+      <a class="app" href="youtube.com"><img src="https://rotony.com.br/wp-content/uploads/2021/09/free-youtube-logo-icon-2431-thumb.png" alt="YT"></a>
+    </div>
+  </div>
+</div>
+
+<style>
+  .btn{
+    background-color: transparent;
+    border-style: none;
+    font-size: 1.3rem;
+    color: white;
+    border-radius: 5px;
+  }
+
+  .btn:hover{
+    background-color: rgba(255, 255, 255, 0.521);
+    color: rgb(255, 0, 0);
+  }
+  .close-btn{
+    height: 10%;
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-end;
+  }
+
+  .img{
+    width: 70%;
+    height: 70%;
+  }
+
+  .app{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .links{
+    height: 90%;
+    display: flex;
+    flex-direction: column;
+    align-items:flex-start;
+    justify-content:flex-start;
+  }
+
+  .bar{
+    background-color: rgba(0, 0, 0, 0.792)  ;  
+    position: fixed;
+    height: 95%;
+    width: 5%;
+    z-index: 1;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
